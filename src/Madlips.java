a9eba2351f89c3772d41a4ccbb24a280660241fb
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-02-20 16:57:55 -0800

    madlips

---
+++ src/Madlips.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Madlips {
+public static void main(String[] args) {
+	
+	String noun="";
+	
+	 noun=JOptionPane.showInputDialog("type a noun");
+	 
+	String verb="";
+	
+	verb=JOptionPane.showInputDialog("Type a verb");
+	
+	String adj="";
+	
+	adj=JOptionPane.showInputDialog("Type a adjective");
+	
+	System.out.println(adj +" "+ noun+" " + verb + "!");
+	
+	
+	
+	
+}
+}
