52d1658e727a1350385d52d0d86a88dbbf3fd364
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-02-27 16:50:55 -0800

    banana split

---
+++ src/banana_split.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class banana_split {
+	public static void main(String[] args) {
+		String crazy="";
+		for(int i=1; i<4; i++){
+		
+		crazy=crazy+"ice cream\n";
+		
+		//System.out.println(crazy);
+		
+		
+	}
+		crazy=crazy+"banana";
+	JOptionPane.showMessageDialog(null, crazy);
+	//void draw(){
+		//for(int i=1; i<4; i++){
+		  //fill(87,104,252);
+		  //text("ice cream", 20, i*20);
+		  
+		//}
+		//text("banana", 20, 80);
+		//}
+}
+}
